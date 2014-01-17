90ec3762a5e46a5f3fb7c16e5ac8cc472b26ac33
siciarek <siciarek@gmail.com>
2014-01-17 19:04:22 +0100

    Fix gui

--- src/com/siciarek/gui/Window.java
+++ src/com/siciarek/gui/Window.java
@@ -1,6 +1,7 @@
 package com.siciarek.gui;
 
 import java.awt.Dimension;
+import java.util.HashMap;
 
 import javax.swing.JFrame;
 
@@ -11,47 +12,49 @@ public class Window extends JFrame {
 	private static final long serialVersionUID = 1398197640669922135L;
 
 	private boolean fullscreen = true;
-	
+
 	public Integer scale = 1;
-	
+
 	public Window() throws InterruptedException {
 
 		this.setScale(2);
-		
+
 		if (this.fullscreen == true) {
 			this.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		} else {
 			this.setSize(640, 480);
 		}
-		
+
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
 		Dimension size = this.getRealSize();
-		
+
+		String name = "Mazectric";
 		CellularAutomaton automaton;
 
-		// Elementary Cellular Automaton:
-		automaton = new ElementaryCellularAutomaton(size.width, size.height, this);
-		automaton.setAutomatonName("Elementary Cellular Automaton");
-		automaton.setDensity(0);
+		if (name == null) {
+			// Elementary Cellular Automaton:
+			automaton = new ElementaryCellularAutomaton(size.width, size.height, this);
+			automaton.setAutomatonName("Elementary Cellular Automaton");
+			automaton.setDensity(0);
 
-		// Automata defined by stringrule:
-		String stringrule;
-		
-		stringrule = "B3/S23";
-		stringrule = "B3/S12345/11";
-		stringrule = "B3/S1234/11";
-		stringrule = "B37/S1234/11";
-		
-		automaton = new CellularAutomaton(size.width, size.height, this, stringrule);
-		automaton.setAutomatonName("Game of Life");
-		int[][] pattern = { { 0, 1 }, { 1, 2 }, { 2, 0 }, { 2, 1 }, { 2, 2 } };
-		automaton.setPattern(pattern);
+		} else {
+			// Automata defined by stringrule:
+			HashMap<String, String> automata = new HashMap<String, String>();
 
+			automata.put("Game of Life", "B3/S23");
+			automata.put("Maze", "B3/S1234/11");
+			automata.put("Maze Full of Mice", "B37/S1234/11");
+			automata.put("Mazectric", "B3/S12345/11");
+
+			automaton = new CellularAutomaton(size.width, size.height, this, automata.get(name));
+			automaton.setAutomatonName(name);
+			int[][] pattern = { { 0, 1 }, { 1, 2 }, { 2, 0 }, { 2, 1 }, { 2, 2 } };
+			automaton.setPattern(pattern);
+		}
 		automaton.setUp();
-		
-		
+
 		this.add(automaton);
 
 		// Animation start:
