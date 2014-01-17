8a93a26128901b5808488c33c08576d979d4bfad
siciarek <siciarek@gmail.com>
2014-01-17 18:00:49 +0100

    initial commit

---
+++ src/com/siciarek/automata/ElementaryCellularAutomaton.java
@@ -0,0 +1,70 @@
+package com.siciarek.automata;
+
+import java.util.Vector;
+
+import com.siciarek.graphics.Window;
+
+public class ElementaryCellularAutomaton extends CellularAutomaton {
+
+	private static final long serialVersionUID = 1L;
+	protected int rule = 30;
+	protected Vector<Integer> rules = new Vector<Integer>(8);
+	protected double density = 0;
+
+	public ElementaryCellularAutomaton(int width, int height, Window window) {
+		super(width, height, window);
+	}
+
+	public String getTitle() {
+		return " " + this.automatonName + " (rule " + rule + ", step " + this.row + "/" + this.rows + ")";
+	}
+
+	public void init() {
+
+		this.row = 0;
+		this.setRules(this.rule);
+		this.clean();
+
+		if (this.density == 0) {
+			this.buffer[this.row][this.cols / 2] = 1;
+		} else {
+			for (int c = 0; c < this.cols; c++) {
+				this.buffer[this.row + 1][c] = Math.random() < this.density ? 0 : 1;
+			}
+		}
+	}
+
+	public void nextPattern() {
+		this.rule = ++this.rule % 256;
+		this.setUp();
+	}
+
+	protected boolean step() {
+
+		for (int c = 0; c < this.cols; c++) {
+			this.buffer[this.row + 1][c] = this.getNextValue(this.row, c);
+		}
+
+		return (++row + 1) < this.rows;
+	}
+
+	protected int getNextValue(int row, int col) {
+
+		int key = 0;
+		int k = 0;
+
+		do {
+			int c = (this.cols + col - k + 1) % this.cols;
+			key |= this.grid[row][c] << k++;
+		} while (k < 3);
+
+		return this.rules.get(key);
+	}
+
+	protected void setRules(int r) {
+		rules = new Vector<Integer>(8);
+		for (int i = 0; i < 8; i++) {
+			this.rules.add((r >> i) % 2);
+		}
+	};
+}
--- src/com/siciarek/graphics/Window.java
+++ src/com/siciarek/graphics/Window.java
@@ -1,41 +1,81 @@
 package com.siciarek.graphics;
+
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
 
-import com.siciarek.automata.ElemantaryCellularAutomaton;
+import com.siciarek.automata.*;
 
 public class Window extends JFrame {
 
-	String title = "Cellular Automaton";
-	Integer minWidth = 800;
-	Integer minHeight = 600;
-	Display display = new Display();
-	ElemantaryCellularAutomaton automaton;
+	private static final long serialVersionUID = 1398197640669922135L;
 
-	private static final long serialVersionUID = -3713765476100328076L;
+	private boolean fullscreen = true;
+	
+	public Integer scale = 1;
+	
+	public Window() throws InterruptedException {
 
-	public void setAutomaton(ElemantaryCellularAutomaton automaton) {
+		this.setScale(2);
+		
+		if (this.fullscreen == true) {
+			this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		} else {
+			this.setSize(640, 480);
+		}
+		
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+		Dimension size = this.getRealSize();
+		
+		CellularAutomaton automaton;
+
+		// Elementary Cellular Automaton:
+		automaton = new ElementaryCellularAutomaton(size.width, size.height, this);
+		automaton.setAutomatonName("Elementary Cellular Automaton");
+		automaton.setDensity(0);
+
+		// Automata defined by stringrule:
+//		String stringrule;
+//		
+//		stringrule = "B3/S23";
+//		stringrule = "B3/S12345/11";
+//		stringrule = "B3/S1234/11";
+//		stringrule = "B37/S1234/11";
+//		
+//		automaton = new CellularAutomaton(size.width, size.height, this, stringrule);
+//		automaton.setAutomatonName("Game of Life");
+//		int[][] pattern = { { 0, 1 }, { 1, 2 }, { 2, 0 }, { 2, 1 }, { 2, 2 } };
+//		automaton.setPattern(pattern);
+
+		automaton.setUp();
+		
+		
+		this.add(automaton);
+
+		// Animation start:
+		while (true) {
+			if (automaton.move() == false) {
+				automaton.nextPattern();
+			}
+			Thread.sleep(100);
+		}
 	}
 
-	public Window(ElemantaryCellularAutomaton automaton) throws InterruptedException   {
-		this.display.automaton = automaton;
-		
-		display.setAutomaton(automaton);
+	/**
+	 * Set scale
+	 * 
+	 * @param scale
+	 */
+	private void setScale(int scale) {
+		this.scale = scale;
+	}
 
-		this.setTitle(this.title);
-		this.setMinimumSize(new Dimension(minWidth, minHeight));
-//		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		this.setLocationRelativeTo(null);
-		
-		this.getContentPane().add(display);
-		this.pack();
-		this.setVisible(true);
+	private Dimension getRealSize() {
+		int width = this.getWidth() - (this.getInsets().left + this.getInsets().right);
+		int height = this.getHeight() - (this.getInsets().top + this.getInsets().bottom);
 
-		while(true) {
-			System.out.print(".");
-			display.repaint();
-			Thread.sleep(500);
-		}
+		return new Dimension(width, height);
 	}
 }
